47a977341f4bcce15af9c51e6c94e388de79aa19
nilesh1935 <nilesh.kulk1234@gmail.com>
2022-04-29 13:27:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-yusn6p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yusn6p)
---
+++ index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+//Find the duplicate characters that are present in the below mentioned string. Also you have to find out the count of each duplicate characters i.e., how many times each duplicate characters are occurring in the string. Finally, your solution should have a time complexity of O(n).
+
+
+
+//AAasdsTERUIfg%#$,gfhADcvbm89ADSasopkJFDP78kfdm^$*(gkfld()()()()
+
+
+//
+// Write Javascript code!
+
+let strName = "AaBB"
+
+function getFrequency(string) {
+  var freq = {};
+  for (var i=0; i<string.length;i++) {
+      var character = string.charAt(i);
+      if (freq[character]) {
+         freq[character]++;
+      } else {
+         freq[character] = 1;
+        
+      }
+  }
+  
+  return freq;
+  };
+
+let finalAnswer = getFrequency(strName)
+console.log(finalAnswer)
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
